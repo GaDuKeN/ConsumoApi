c05e3239bc4cdcca743b06777f0476ade3de512b
GABRIEL DO NASCIMENTO QUINTILIANO <gabriel.quintiliano@GRANBERY.local>
2024-10-03 20:23:07 -0300

    Sincronizando com o git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConversorMoedasComponent } from './conversor-moedas/conversor-moedas.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversorMoedasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
